64ae3da7198fb854f5cd3c836cf800c209b0f77c
DARK5X <loran31@live.com>
2020-12-08 21:41:30 +0300

    Add files via upload

---
+++ pdf to excel.py
@@ -0,0 +1,72 @@
+import xlsxwriter
+import pandas as pd
+import openpyxl
+#--------------
+from Tkinter import * 
+import tkFileDialog
+
+#-----------------
+from pdfminer.layout import LAParams, LTTextBox
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+
+#------------------------------
+import sys
+import os
+if sys.version_info[0] < 3:
+   import Tkinter as Tk
+else:
+   import tkinter as Tk
+
+#--------------------------------
+def browse_file():
+    fname = tkFileDialog.askopenfilename(filetypes = (("pdf files", "pdf"), ("All files", "*")))
+
+    fp = open(fname, 'rb')
+    rsrcmgr = PDFResourceManager()
+    laparams = LAParams()
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    pages = PDFPage.get_pages(fp)
+    ls=[]
+    for page in pages:
+        interpreter.process_page(page)
+        layout = device.get_result()
+        for lobj in layout:
+            if isinstance(lobj, LTTextBox):
+                x, y, text = lobj.bbox[0], lobj.bbox[3], lobj.get_text()
+                print(x,y,text)
+               #after you knew what are the values of the wanted X's and Y's
+                #copy and paste the values bellow then comment the first print and
+                #uncomment the second one to have a clear view of the resulat
+                if x==9.6681 and y==74.58526499999999:
+                    #print(text)
+                    ls.append(text)
+   #here is ex. of having more than one col
+    #f=[1, 2, 3, 4, 5, 6]
+    df = pd.DataFrame() 
+# Creating two columns 
+    df['Col1'] = ls
+    #df['col2'] = f 
+  
+# Converting to excel
+    xlname='result.xlsx'
+    df.to_excel(xlname,sheet_name="sheetname", index = False)
+   
+    
+    print("Done Transfering")
+
+
+root=Tk.Tk()
+root.wm_title("Browser")
+broButton = Tk.Button(master = root, text = 'Browse', width = 10, fg='red',command=browse_file)
+broButton.pack(side=Tk.BOTTOM, padx = 2, pady=2)
+root.mainloop()
+
+
+
+
+
+        
---
+++ pdf to word.py
@@ -0,0 +1,61 @@
+from docx import Document
+import sys
+import os
+#-----------------
+from pdfminer.layout import LAParams, LTTextBox
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+
+#------------------------------
+from Tkinter import * 
+import tkFileDialog
+if sys.version_info[0] < 3:
+   import Tkinter as Tk
+else:
+   import tkinter as Tk
+
+#------------------------------
+document = Document()
+document.add_heading('The Title: ', 0)
+#------------------------------------
+def browse_file():
+    fname = tkFileDialog.askopenfilename(filetypes = (("pdf files", "pdf"), ("All files", "*")))
+    fp = open(fname, 'rb')
+    rsrcmgr = PDFResourceManager()
+    laparams = LAParams()
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    pages = PDFPage.get_pages(fp)
+    ls=[]
+    for page in pages:
+        interpreter.process_page(page)
+        layout = device.get_result()
+        for lobj in layout:
+            if isinstance(lobj, LTTextBox):
+                x, y, text = lobj.bbox[0], lobj.bbox[3], lobj.get_text()
+                print(x,y,text)
+                #after you knew what are the values of the wanted X's and Y's
+                #copy and paste the values bellow then comment the first print and
+                #uncomment the second one to have a clear view of the resulat
+                if x==9.6681 and y==74.58526499999999:
+                    #print(text)
+                    ls.append(text)
+#------------------------------------
+
+#-----------------------------
+#if u have a Serails you can use this part
+#for i in ls:
+    #paragraph =document.add_paragraph("Serail: "+i)
+    
+#------------------------------
+    for i in ls:
+        paragraph =document.add_paragraph(i)
+    document.save('output.docx')
+
+root=Tk.Tk()
+root.wm_title("Browser")
+broButton = Tk.Button(master = root, text = 'Browse', width = 10, fg='red',command=browse_file)
+broButton.pack(side=Tk.BOTTOM, padx = 2, pady=2)
+root.mainloop()
